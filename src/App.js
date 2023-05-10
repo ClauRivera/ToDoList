882eb84f1f80815820000405b588e5b29d54cc33
Claudia Rivera <claudiariveradev@gmail.com>
2023-05-09 22:32:19 -0300

    Lista de tareas en React en proceso

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+<h1>LISTA DE TAREAS</h1>
+      </div>
+        <div className='tareas-lista-principal'>
+          <ListaDeTareas />
+        </div>
     </div>
   );
 }
